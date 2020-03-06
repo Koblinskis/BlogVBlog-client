cadf0dc50e147657beceb63895ef7f773f73951b
Michael Koblinski <stevenkoblinskiw@gmail.com>
2020-03-06 13:31:39 -0500

    fixed text

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => (
       <p>This webpage allows people to vote on titles of blogs they find most interesting.</p>
       <p>Vote for the title that seems the most appeasing.</p>
       <p className="home__small-text">Created by S. Michael Koblinski</p>
-      <p className="home__small-text">Uses MongoDB for a server. <br/>React for the frontend, and <br/>NodeJS for the backend</p>
+      <p className="home__small-text">Uses MongoDB for the database. <br/>React for the frontend, and <br/>NodeJS for the backend</p>
     </div>
     <button className="home__button">
       <NavLink to="/versus" className="home__button-text">Versus Page</NavLink>

858e98286dd1e80eb6279f811170580fc6f13c0c
Michael Koblinski <stevenkoblinskiw@gmail.com>
2020-03-06 09:07:42 -0500

    isolated client

--- blogvblog/server/database/mongodb.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.REACT_APP_MONGODB_URL, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-})
--- blogvblog/server/routes/timeline.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = new express.Router()
-const timeline_controller = require('../controllers/timelineController')
-
-router.post('/timeline', timeline_controller.timeline_create_post)
-
-module.exports = router
